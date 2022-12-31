99e4be6088f896c1e67b07066d52bd7ebb2675b9
ductamisme <laymaynhanhdumtaocai@gmail.com>
2022-12-31 16:20:50 +0700

    i knew it

--- DemoIntentJava/app/src/main/java/com/example/demointentjava/SecondActivity.java
+++ DemoIntentJava/app/src/main/java/com/example/demointentjava/SecondActivity.java
@@ -1,2 +1,39 @@
-package com.example.demointentjava;public class SecondActivity {
+package com.example.demointentjava;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    Button btn_back;
+    Button btn_finish;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        btn_finish = findViewById(R.id.btn_finish);
+        btn_back = findViewById(R.id.btn_back);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent back = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(back);
+            }
+        });
+
+        btn_finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 }
